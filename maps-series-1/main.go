96b2139b1c5834e2e93d28b61a4df221252fbc54
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-09-24 08:21:05 +0100

    check for the existence of a value in a map

--- maps-series-1/main.go
+++ maps-series-1/main.go
@@ -16,8 +16,17 @@ func main() {
 	eggsPrice := menu["eggs"]
 	fmt.Printf("eggsPrice: %v\n", eggsPrice)
 
-	// to change the price of eggs say to 1.50; use the name of the map and then the key and pass in the desired value
+	// to change the price of eggs say to 1.50; use the name of the map with the key as an index and pass in the desired value
 	menu["eggs"] = 1.50
-	fmt.Printf("menu:%#v", menu)
+	fmt.Printf("menu:%#v\n", menu)
+
+	// to check if a key exists
+	value, ok := menu["hash_brown"]
+	if ok {
+		fmt.Println("hash_brown exists!")
+	}
+
+	// if it does not exist, the value variable above is defaulted to its respective zero-type
+	fmt.Printf("value is %v and of type %T", value, value)
 
 }
