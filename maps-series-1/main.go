5e53c357c9b7dcd10011c1b695b635f1684a2d8c
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-09-24 08:12:00 +0100

    changing the value of a map

--- maps-series-1/main.go
+++ maps-series-1/main.go
@@ -12,8 +12,12 @@ func main() {
 		"sausage": 1.89,
 	}
 
-	// to retrieve the price of eggs; use the name of the map and the key
+	// to retrieve the price of eggs; use the name of the map and the key to access
 	eggsPrice := menu["eggs"]
 	fmt.Printf("eggsPrice: %v\n", eggsPrice)
+
+	// to change the price of eggs say to 1.50; use the name of the map and then the key and pass in the desired value
+	menu["eggs"] = 1.50
+	fmt.Printf("menu:%#v", menu)
 
 }
