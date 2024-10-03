59dd8b01f0da3e191403a2011727553cc5d33652
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-10-03 11:32:56 +0100

    range through the value of the map only

--- maps-series-4/main.go
+++ maps-series-4/main.go
@@ -16,4 +16,12 @@ func main() {
 	for dish := range menu {
 		fmt.Println("Please remember to get", dish)
 	}
+
+	// if we want only the second value of the map, we use a blank identifier to replace the key
+	var total float64
+	for _, price := range menu {
+		total += price
+	}
+
+	fmt.Printf("An everything breakfast will cost you %.02f", total)
 }
