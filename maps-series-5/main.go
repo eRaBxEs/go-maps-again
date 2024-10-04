a6281d257fb62964ff0a682936bf580d30dfe85f
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-10-04 13:54:18 +0100

    use type assertion to determine the type of a value in a map of string interface

--- maps-series-5/main.go
+++ maps-series-5/main.go
@@ -16,10 +16,26 @@ func main() {
 			1.75,
 		},
 		"steak": true,
+		"rice":  5,
+		"beans": 6.78,
 	}
 
 	// loop through the map above and print the values
 	for k, v := range foods {
 		fmt.Printf("The string key %q has a value of %v of type %T\n", k, v, v)
+	}
+
+	fmt.Println()
+
+	// because of the nature of the type of a map[string]interface, use type assertion while looping
+	for k, v := range foods {
+		switch c := v.(type) {
+		case string:
+			fmt.Printf("Item %q is a string, containing %q\n", k, c)
+		case float64:
+			fmt.Printf("Looks like item %q is a number, specifically %f\n", k, c)
+		default:
+			fmt.Printf("Not sure what type item %q is, but I think it might be %T\n", k, c)
+		}
 	}
 }
