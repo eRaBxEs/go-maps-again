2198f32a00fd5f426879aec1b7ac30b1d0d52465
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-10-04 17:57:35 +0100

    To identify empty maps

---
+++ maps-series-6/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// To check if any map is empty; get the length and if it is zero, map is empty
+	m := map[string]bool{
+		"Go is awesome":             true,
+		"I drink and I know things": true,
+		"PI IS EXACTLY THREE!":      false,
+	}
+	fmt.Println(len(m))
+
+	emptyMapOne := map[string]bool{}
+	emptyMapTwo := make(map[string]bool)
+
+	fmt.Println("emptyMapOne has a length of", len(emptyMapOne))
+	fmt.Println("emptyMapTwo has a length of", len(emptyMapTwo))
+
+}
